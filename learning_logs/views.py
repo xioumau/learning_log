e9cc7657f7d4c6085fb017657f1727309b12f23b
mauricio <mauriciocon@gmail.com>
2021-07-21 22:14:26 -0300

    Throw error for a nonexistent object

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -22,7 +22,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Mostra um único assunto e todas as suas entradas"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # Garante que o assunto pertence ao usuário atual
     if topic.owner != request.user:
         raise Http404
